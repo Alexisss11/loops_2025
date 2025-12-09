2225a4f2784e7e929056a620edc7ca27440e1b5b
Alexisss11 <alexispurple1978@gmail.com>
2025-12-09 20:21:53 +0000

    pratcice

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,18 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+# you can irritate anyth8gng that ius irrtable (anything you cna ;loop)
+# for loops = exexute a blocki of code a fixed number of times
+# for x in range (0, 11, 2):
+#     print (x) 
+
+#     print ("Happy New Year")
+
+# credit_card = "1234-5678-9012-3456"
+# for x in credit_card:
+#     print(x)
+
+# for x in range(1 ,21):
+#     if x ==13:
+#         break
+#     else:
+#         print(x)
